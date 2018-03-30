b20e4a5b0edd8f2d036ef22e08fc295b33c0d6c5
Michael Romano <micha7a.com@gmail.com>
2018-03-30 04:29:33 -0400

    Add api to lib

---
+++ lib/google_maps_geocode_api.rb
@@ -0,0 +1,34 @@
+class GoogleMapsGeocodeApi
+  include HTTParty
+  base_uri 'https://maps.googleapis.com/maps/api/'.freeze
+
+  def initialize(location_params)
+    @response_format = :json
+    @options = {
+      query: {
+        address: location_params[:location_address],
+        key: 'AIzaSyDFdBnMHE8sqdUl3VBse7bXRWpV0XPC5pY'
+      }
+    }
+  end
+
+  def formatted_addresses_and_locations
+    @formatted_addresses ||= results.map do |result|
+      {
+        location_address: result.dig('formatted_address'),
+        location_lonlat:
+          "POINT (#{result.dig('geometry', 'location').values.join(' ')})"
+      }
+    end
+  end
+
+  private
+
+  def results
+    @results ||= geocoded_address.dig('results')
+  end
+
+  def geocoded_address
+    @geocoded_address ||= self.class.get("/geocode/#{@response_format}", @options)
+  end
+end
